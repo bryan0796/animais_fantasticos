ab42037c0ec16b34f9438d7c9d8ab5f8184a5f20
Bryan de Palma Martins Vida <bpmv96@outlook.com>
2022-11-04 21:27:36 -0300

    js

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
-const animais = document.getElementById('animais');
-//console.log(animais);
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-const gridSection = document.getElementsByClassName('grid-section');
-//console.log(gridSection[1]);
+if(tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add('ativo');
 
-const primeiraLi = document.querySelector('li');
-//console.log(primeiraLi);
+  function activeTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove('ativo');
+    })
+    tabContent[index].classList.add('ativo');
+  };
 
-const primeiraUl = document.querySelector('ul');
-//console.log(primeiraUl);
-
-const linkInterno = document.querySelector('[href^="#"]');
-//console.log(linkInterno);
-
-const animaisImg = document.querySelectorAll('.animais img');
-//console.log(animaisImg[1]);
-
-const gridSectionHTMl = document.getElementsByClassName('grid-section');
-const gridSectionNode = document.querySelectorAll('.grid-section');
-
-primeiraUl.classList.add('grid-section');
-
-//console.log(gridSectionHTMl);
-//console.log(gridSectionNode);
-
-gridSectionNode.forEach(function(item, index){
-  console.log(item);
-});
-
-const arrayGrid = Array.from(gridSectionHTMl);
-arrayGrid.forEach(function(item) {
-  console.log(item);
-})
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', function() {
+      activeTab(index);
+    })
+  });
+}
